bd1e29a86db1b5857b16a405bd82c987b0d51091
Ali Rostami <rostamiev@gmail.com>
2023-09-30 18:20:14 +0000

    making d3

--- graphs/index.js
+++ graphs/index.js
@@ -41,66 +41,68 @@ function convertToCytoscapeGraph(edgeString) {
 }
 
 var uuid = guid();
-var type = $('#graphType').find('option:selected').text();
+var type = d3.select('#graphType').property('value');
 mycys = [];
 
 function send() {
     var strings = document.getElementById("strings").value;
-    $('#mytable').empty();
-    var mytable =$('#mytable');
+    d3.select('#mytable').html(''); // Clear the content
+    var mytable = d3.select('#mytable');
     var str = "";
     var cnt = 0;
     var cys = [];
     strings.split(',').forEach(function(d) {
-        str+="<div id='g" + cnt + "'></div>";
+        str += "<div id='g" + cnt + "'></div>";
         cnt++;
     });
-    mytable.append(str);
+    mytable.html(str);
     cnt = 0;
     strings.split(',').forEach(function(d) {
-        var res = passStringToWasm(d);
-        var tmpcy = cytoscape({
-            container: document.getElementById("g" + cnt),
-            style: [ 
-                {
-                    selector: 'node',
-                    style: {
-                        'background-color': 'lightgray',
-                        'label': 'data(label)',
-                        'text-valign': 'center',
-                        'background-opacity': 0.7,
-                        'border-width': 1,
-                        'border-color': 'black',
-                        'label': function(ele) {
-                                return (parseInt(ele.id()) + 1).toString();
+        strings.split(',').forEach(function(d) {
+            var res = passStringToWasm(d);
+            var tmpcy = cytoscape({
+                container: document.getElementById("g" + cnt),
+                style: [ 
+                    {
+                        selector: 'node',
+                        style: {
+                            'background-color': 'lightgray',
+                            'label': 'data(label)',
+                            'text-valign': 'center',
+                            'background-opacity': 0.7,
+                            'border-width': 1,
+                            'border-color': 'black',
+                            'label': function(ele) {
+                                    return (parseInt(ele.id()) + 1).toString();
+                            }
                         }
-                    }
-                },
-                {
-                    selector: '.selected',
-                    style: {
-                        'background-color': 'rgba(150, 211, 255,0.6)',
-                        'label': 'data(label)',
-                        'text-valign': 'center'
-                    }
-                },
-                {
-                    selector: 'edge',
-                    style: {
-                        'curve-style': 'bezier'
-                        // 'target-arrow-shape': arrow
-                    }
-                }]
+                    },
+                    {
+                        selector: '.selected',
+                        style: {
+                            'background-color': 'rgba(150, 211, 255,0.6)',
+                            'label': 'data(label)',
+                            'text-valign': 'center'
+                        }
+                    },
+                    {
+                        selector: 'edge',
+                        style: {
+                            'curve-style': 'bezier'
+                            // 'target-arrow-shape': arrow
+                        }
+                    }]
+            });
+            cys.push(tmpcy);
+            const graph = convertToCytoscapeGraph(res);
+            var nodes = graph.nodes;
+            var edges = graph.edges;
+            tmpcy.elements().remove();
+            tmpcy.add(nodes);
+            tmpcy.add(edges);
+            cnt = cnt + 1;
         });
-        cys.push(tmpcy);
-        const graph = convertToCytoscapeGraph(res);
-        var nodes = graph.nodes;
-        var edges = graph.edges;
-        tmpcy.elements().remove();
-        tmpcy.add(nodes);
-        tmpcy.add(edges);
-        cnt = cnt + 1;
-    });
+    
 
     cys.forEach(function (c) {
         c.layout({name:"cose"}).run();
@@ -111,47 +113,41 @@ function send() {
 
 function create_images() {
     let cnt = 0;
-    $.each(mycys, function(index, c) {
+    mycys.forEach(function(c) {
         const imgURI = c.png();
-        const containerDiv = $("<div>").addClass("image-container");
+        const containerDiv = d3.select("#imageContainer").append("div").attr("class", "image-container");
 
-        $("<img>").attr("src", imgURI).appendTo(containerDiv);
+        containerDiv.append("img").attr("src", imgURI);
 
-        $("<span>").addClass("graph-name-label").text(`G${cnt+1}`).appendTo(containerDiv);
+        containerDiv.append("span").attr("class", "graph-name-label").text(`G${cnt+1}`);
 
-        $("#imageContainer").append(containerDiv);
-        $("#g" + cnt).hide();
+        d3.select(`#g${cnt}`).style("display", "none");
         cnt++;
     });
 
-        const newWindow = window.open("", "_blank");
-        const table = $("<table>").css({
-            borderCollapse: 'collapse',
-            width: '100%'
+    const newWindow = window.open("", "_blank");
+    const table = d3.select(newWindow.document.body).append("table").style({
+        borderCollapse: 'collapse',
+        width: '100%'
+    });
+
+    const headerRow = table.append("tr");
+    headerRow.append("th");
+    cnt = 0;
+    mycys.forEach(function() {
+        headerRow.append("th").text(`G${++cnt}`);
+    });
+
+    cnt = 0;
+    mycys.forEach(function() {
+        const row = table.append("tr");
+        row.append("th").text(`G${++cnt}`);
+        mycys.forEach(function() {
+            row.append("td").attr("class", "table-cell").text('1');
         });
-    
-        const headerRow = $("<tr>");
-        headerRow.append($("<th>")); 
-        cnt = 0;
-        $.each(mycys, function() {
-            headerRow.append($("<th>").text(`G${++cnt}`));
-        });
-        table.append(headerRow);
-    
-        cnt = 0;
-        $.each(mycys, function() {
-            const row = $("<tr>");
-            row.append($("<th>").text(`G${++cnt}`));
-            $.each(mycys, function() {
-                row.append($("<td>").addClass("table-cell").text('1'));
-            });
-            table.append(row);
-        });
-    
-        newWindow.document.write("<html><head><title>Graph Table</title>");
-        newWindow.document.write('<link rel="stylesheet" type="text/css" href="tableStyles.css">'); // Link the external CSS
-        newWindow.document.write("</head><body>");
-        newWindow.document.write(table.prop('outerHTML'));
-        newWindow.document.write("</body></html>");
-        newWindow.document.close(); 
+    });
+
+    newWindow.document.write("<head><title>Graph Table</title>");
+    newWindow.document.write('<link rel="stylesheet" type="text/css" href="tableStyles.css">'); // Link the external CSS
+    newWindow.document.write("</head>");
 }
