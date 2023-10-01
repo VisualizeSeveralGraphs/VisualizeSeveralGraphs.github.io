49d431c86b336050f9853de01277fa40fae647be
Ali Rostami <rostamiev@gmail.com>
2023-10-01 07:11:07 +0200

    Revert "making d3"
    This reverts commit bd1e29a86db1b5857b16a405bd82c987b0d51091.

--- graphs/index.js
+++ graphs/index.js
@@ -11,7 +11,8 @@ function passStringToWasm(str) {
 }
 
 function convertToCytoscapeGraph(edgeString) {
-    const edgesArray = edgeString.split("--").slice(0, -1); 
+    // Split the string by "--" to get individual edges
+    const edgesArray = edgeString.split("--").slice(0, -1); // Remove the last empty element due to extra "--"
 
     const nodes = {};
     const edges = [];
@@ -39,76 +40,67 @@ function convertToCytoscapeGraph(edgeString) {
     };
 }
 
+var uuid = guid();
+var type = $('#graphType').find('option:selected').text();
 mycys = [];
-
-function addElements() {
-    var strings = document.getElementById("strings").value;
-    var cnt = 0;
-    strings.split(',').forEach(function(d) {
-        mytable.append("div").attr("id", "g" + cnt);
-        cnt++;
-    });
-}
 
 function send() {
     var strings = document.getElementById("strings").value;
-    d3.select('#mytable').html(''); 
-    var mytable = d3.select('#mytable');
+    $('#mytable').empty();
+    var mytable =$('#mytable');
     var str = "";
     var cnt = 0;
     var cys = [];
-    // strings.split(',').forEach(function(d) {
-    //     mytable.append("div").attr("id", "g" + cnt);
-    //     cnt++;
-    // });
+    strings.split(',').forEach(function(d) {
+        str+="<div id='g" + cnt + "'></div>";
+        cnt++;
+    });
+    mytable.append(str);
     cnt = 0;
     strings.split(',').forEach(function(d) {
-        strings.split(',').forEach(function(d) {
-            var res = passStringToWasm(d);
-            var tmpcy = cytoscape({
-                container: d3.select("#g" + cnt),
-                style: [ 
-                    {
-                        selector: 'node',
-                        style: {
-                            'background-color': 'lightgray',
-                            'label': 'data(label)',
-                            'text-valign': 'center',
-                            'background-opacity': 0.7,
-                            'border-width': 1,
-                            'border-color': 'black',
-                            'label': function(ele) {
-                                    return (parseInt(ele.id()) + 1).toString();
-                            }
+        var res = passStringToWasm(d);
+        var tmpcy = cytoscape({
+            container: document.getElementById("g" + cnt),
+            style: [ 
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': 'lightgray',
+                        'label': 'data(label)',
+                        'text-valign': 'center',
+                        'background-opacity': 0.7,
+                        'border-width': 1,
+                        'border-color': 'black',
+                        'label': function(ele) {
+                                return (parseInt(ele.id()) + 1).toString();
                         }
-                    },
-                    {
-                        selector: '.selected',
-                        style: {
-                            'background-color': 'rgba(150, 211, 255,0.6)',
-                            'label': 'data(label)',
-                            'text-valign': 'center'
-                        }
-                    },
-                    {
-                        selector: 'edge',
-                        style: {
-                            'curve-style': 'bezier'
-                            // 'target-arrow-shape': arrow
-                        }
-                    }]
-            });
-            cys.push(tmpcy);
-            const graph = convertToCytoscapeGraph(res);
-            var nodes = graph.nodes;
-            var edges = graph.edges;
-            tmpcy.elements().remove();
-            tmpcy.add(nodes);
-            tmpcy.add(edges);
-            cnt = cnt + 1;
+                    }
+                },
+                {
+                    selector: '.selected',
+                    style: {
+                        'background-color': 'rgba(150, 211, 255,0.6)',
+                        'label': 'data(label)',
+                        'text-valign': 'center'
+                    }
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'curve-style': 'bezier'
+                        // 'target-arrow-shape': arrow
+                    }
+                }]
         });
+        cys.push(tmpcy);
+        const graph = convertToCytoscapeGraph(res);
+        var nodes = graph.nodes;
+        var edges = graph.edges;
+        tmpcy.elements().remove();
+        tmpcy.add(nodes);
+        tmpcy.add(edges);
+        cnt = cnt + 1;
     });
-    
 
     cys.forEach(function (c) {
         c.layout({name:"cose"}).run();
@@ -119,41 +111,47 @@ function send() {
 
 function create_images() {
     let cnt = 0;
-    mycys.forEach(function(c) {
+    $.each(mycys, function(index, c) {
         const imgURI = c.png();
-        const containerDiv = d3.select("#imageContainer").append("div").attr("class", "image-container");
+        const containerDiv = $("<div>").addClass("image-container");
 
-        containerDiv.append("img").attr("src", imgURI);
+        $("<img>").attr("src", imgURI).appendTo(containerDiv);
 
-        containerDiv.append("span").attr("class", "graph-name-label").text(`G${cnt+1}`);
+        $("<span>").addClass("graph-name-label").text(`G${cnt+1}`).appendTo(containerDiv);
 
-        d3.select(`#g${cnt}`).style("display", "none");
+        $("#imageContainer").append(containerDiv);
+        $("#g" + cnt).hide();
         cnt++;
     });
 
-    const newWindow = window.open("", "_blank");
-    const table = d3.select(newWindow.document.body).append("table").style({
-        borderCollapse: 'collapse',
-        width: '100%'
-    });
-
-    const headerRow = table.append("tr");
-    headerRow.append("th");
-    cnt = 0;
-    mycys.forEach(function() {
-        headerRow.append("th").text(`G${++cnt}`);
-    });
-
-    cnt = 0;
-    mycys.forEach(function() {
-        const row = table.append("tr");
-        row.append("th").text(`G${++cnt}`);
-        mycys.forEach(function() {
-            row.append("td").attr("class", "table-cell").text('1');
+        const newWindow = window.open("", "_blank");
+        const table = $("<table>").css({
+            borderCollapse: 'collapse',
+            width: '100%'
         });
-    });
-
-    newWindow.document.write("<head><title>Graph Table</title>");
-    newWindow.document.write('<link rel="stylesheet" type="text/css" href="tableStyles.css">'); // Link the external CSS
-    newWindow.document.write("</head>");
+    
+        const headerRow = $("<tr>");
+        headerRow.append($("<th>")); 
+        cnt = 0;
+        $.each(mycys, function() {
+            headerRow.append($("<th>").text(`G${++cnt}`));
+        });
+        table.append(headerRow);
+    
+        cnt = 0;
+        $.each(mycys, function() {
+            const row = $("<tr>");
+            row.append($("<th>").text(`G${++cnt}`));
+            $.each(mycys, function() {
+                row.append($("<td>").addClass("table-cell").text('1'));
+            });
+            table.append(row);
+        });
+    
+        newWindow.document.write("<html><head><title>Graph Table</title>");
+        newWindow.document.write('<link rel="stylesheet" type="text/css" href="tableStyles.css">'); // Link the external CSS
+        newWindow.document.write("</head><body>");
+        newWindow.document.write(table.prop('outerHTML'));
+        newWindow.document.write("</body></html>");
+        newWindow.document.close(); 
 }
